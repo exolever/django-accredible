9241c1e24a72f39cee15d849a87cb5a3250ea25a
Javier Sújar <javier.sujar@gmail.com>
2022-03-04 12:06:15 +0100

    Update Celery registration tasks

--- certification/tasks/__init__.py
+++ certification/tasks/__init__.py
@@ -1,3 +1,5 @@
+import celery
+
 from celery import current_app as app
 
 from .create_accredible_group import CreateAccredibleGroupTask  # noqa
@@ -7,9 +9,18 @@ from .create_accredible_group_credential import CreateAccredibleGroupCredentialT
 from .delete_accredible_group import DeleteAccredibleGroupTask  # noqa
 from .update_accredible_group import UpdateAccredibleGroupTask  # noqa
 
-app.tasks.register(CreateAccredibleGroupTask())
-app.tasks.register(CreateAccredibleConsultantRoleGroupTask())
-app.tasks.register(CreateAccredibleSimpleCredentialTask())
-app.tasks.register(CreateAccredibleGroupCredentialTask())
-app.tasks.register(DeleteAccredibleGroupTask())
-app.tasks.register(UpdateAccredibleGroupTask())
+if '5.2' in celery.__version__:
+    app.register_task(CreateAccredibleGroupTask())
+    app.register_task(CreateAccredibleConsultantRoleGroupTask())
+    app.register_task(CreateAccredibleSimpleCredentialTask())
+    app.register_task(CreateAccredibleGroupCredentialTask())
+    app.register_task(DeleteAccredibleGroupTask())
+    app.register_task(UpdateAccredibleGroupTask())
+
+else:
+    app.tasks.register(CreateAccredibleGroupTask())
+    app.tasks.register(CreateAccredibleConsultantRoleGroupTask())
+    app.tasks.register(CreateAccredibleSimpleCredentialTask())
+    app.tasks.register(CreateAccredibleGroupCredentialTask())
+    app.tasks.register(DeleteAccredibleGroupTask())
+    app.tasks.register(UpdateAccredibleGroupTask())
